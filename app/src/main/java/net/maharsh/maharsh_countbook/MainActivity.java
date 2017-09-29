796bcb029ce279f1a5ce19184cfb171557ba591b
Maharsh Patel <patel@maharsh.net>
2017-09-29 11:41:03 -0600

    alert dialog for new counter

--- app/src/main/java/net/maharsh/maharsh_countbook/MainActivity.java
+++ app/src/main/java/net/maharsh/maharsh_countbook/MainActivity.java
@@ -1,15 +1,19 @@
 package net.maharsh.maharsh_countbook;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -54,14 +58,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Testing", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                System.out.println("FAB Clicked");
-                // add new counter
-                Counter c = new Counter("Counter 1", 0, "comment");
-                counters.add(c);
-                System.out.println("First: " + counters.size());
-
-                counterAdapter.notifyDataSetChanged();
-
+                showNewCounterDialog();
             }
         });
 
@@ -87,5 +84,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showNewCounterDialog(){
+        // set up the alert dialog
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText counter_name = (EditText) promptView.findViewById(R.id.counter_name_input);
+        final EditText counter_value = (EditText) promptView.findViewById(R.id.counter_value_input);
+        final EditText counter_comment = (EditText) promptView.findViewById(R.id.counter_comment_input);
+
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.out.println("OK" + counter_name.getText() + counter_value.getText() + counter_comment.getText());
+                // add the new counter
+                Counter c = new Counter(counter_name.getText().toString(),
+                                        Integer.parseInt(counter_value.getText().toString()),
+                                        counter_comment.getText().toString());
+                counters.add(c);
+                counterAdapter.notifyDataSetChanged();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.out.println("Cancel");
+            }
+        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+
     }
 }
