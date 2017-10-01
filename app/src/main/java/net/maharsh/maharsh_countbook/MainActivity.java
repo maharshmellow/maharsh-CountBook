99c23e80def6aac82385ab25fdac499231968b64
Maharsh Patel <patel@maharsh.net>
2017-09-30 18:03:29 -0600

    lots of design changes

--- app/src/main/java/net/maharsh/maharsh_countbook/MainActivity.java
+++ app/src/main/java/net/maharsh/maharsh_countbook/MainActivity.java
@@ -29,14 +29,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button addButton = (Button) findViewById(R.id.add_counter_button);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        toolbar.setTitle("New Title");
 
         countersListView = (ListView) findViewById(R.id.counters);
         // the textfield at the bottom of the page that shows the total count of counters
         totalCountersField = (TextView) findViewById(R.id.total_counters_field);
+
 
 //        ArrayList<Counter> counterList = new ArrayList<>();
         counters.add(new Counter("Counter 1", 0, "comment"));
@@ -51,38 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
         final Dialog d = new Dialog(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 d.newCounterDialog();
-                // update the total count of counters at the bottom of the screen
-//                totalCountersField.setText(counters.size() + " counters");
-
             }
         });
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
